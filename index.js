e69c8bc239e5d99c63e26e0c1ac27251782f7ec8
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-13 09:42:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // Write your code here
 class Breakfast{
-  Breakfast(food,drink){
+  constructor(food,drink){
     this.food=food;
     this.drink=drink;
   }
 }
 class Lunch{
-  Lunch(salad,soup,drink){
+  constructor(salad,soup,drink){
     this.salad=salad;
     this.soup=soup;
     this.drink=drink;
   }}
   class Dinner{
-  Dinner(salad,soup,entree,_dessert){
+  constructor(salad,soup,entree,_dessert){
     this.salad=salad;
     this.soup=soup;
     this.entree=entree;
