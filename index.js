ced595754ba4cb9c7e640e59c062e1c3ba79e0f8
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-13 09:35:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,18 +5,12 @@ class Breakfast{
     this.drink=drink;
   }
 }
-class Breakfast{
-  Breakfast(food,drink){
-    this.food=food;
-    this.drink=drink;
-  }
-}
 class Lunch{
   Lunch(salad,soup,drink){
     this.salad=salad;
     this.soup=soup;
     this.drink=drink;
-  }
+  }}
   class Dinner{
   Dinner(salad,soup,entree,_dessert){
     this.salad=salad;
